d4904bbc4db8c1b981fefc4605ae12fa69fbcb40
JambonPasFrais <60687226+JambonPasFrais@users.noreply.github.com>
2021-03-24 13:02:57 +0100

    Beginning of TP 6

---
+++ TP6 - Planning Optimisation/Main prog/graphe.cpp
@@ -0,0 +1,5 @@
+#include "graphe.h"
+
+graphe::graphe() {
+	this->grapheEtCapacites[0].push_back(3);
+}
---
+++ TP6 - Planning Optimisation/Main prog/graphe.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class graphe
+{
+private:
+	vector<vector<int>>grapheEtCapacites;
+public:
+	graphe();
+};
+
